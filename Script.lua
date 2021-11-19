b29d878a42275b187621f9879004809fcdcde0fc
Weston7843 <94673101+Weston7843@users.noreply.github.com>
2021-11-19 01:10:08 -0600

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,408 @@
-print("Lua Obfuscator by Pyan")
+--Made By Weston7843 | 7843 Xploits 
+local h7843XploitsMM2 = Instance.new("ScreenGui")
+local loginframe = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local logintitle = Instance.new("TextLabel")
+local UIGradient = Instance.new("UIGradient")
+local logintitle2 = Instance.new("TextLabel")
+local user = Instance.new("TextBox")
+local UICorner_2 = Instance.new("UICorner")
+local DropShadow1 = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
+local pass = Instance.new("TextBox")
+local UICorner_4 = Instance.new("UICorner")
+local DropShadow1_2 = Instance.new("Frame")
+local UICorner_5 = Instance.new("UICorner")
+local buttonlog = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local DropShadow3 = Instance.new("Frame")
+local UICorner_7 = Instance.new("UICorner")
+local openlogin = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local UIGradient_2 = Instance.new("UIGradient")
+local openmm2hax = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local UIGradient_3 = Instance.new("UIGradient")
+local h7843Xploitsmain = Instance.new("Frame")
+local UICorner_10 = Instance.new("UICorner")
+local UIGradient_4 = Instance.new("UIGradient")
+local h7843xploitstitle = Instance.new("TextLabel")
+local credits = Instance.new("TextLabel")
+local eclipse = Instance.new("TextButton")
+local UICorner_11 = Instance.new("UICorner")
+local vynixu = Instance.new("TextButton")
+local UICorner_12 = Instance.new("UICorner")
+local infiniteyeild = Instance.new("TextButton")
+local UICorner_13 = Instance.new("UICorner")
+local emotes = Instance.new("TextButton")
+local UICorner_14 = Instance.new("UICorner")
+
+--Properties:
+
+h7843XploitsMM2.Name = "h7843XploitsMM2"
+h7843XploitsMM2.Parent = game.CoreGui
+
+loginframe.Name = "loginframe"
+loginframe.Parent = h7843XploitsMM2
+loginframe.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+loginframe.Position = UDim2.new(-0.688000023, 0, 0.286000013, 0)
+loginframe.Size = UDim2.new(0, 222, 0, 275)
+
+UICorner.Parent = loginframe
+
+logintitle.Name = "logintitle"
+logintitle.Parent = loginframe
+logintitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+logintitle.BackgroundTransparency = 1.000
+logintitle.Size = UDim2.new(0, 222, 0, 44)
+logintitle.Font = Enum.Font.SciFi
+logintitle.Text = "7843 Xploits MM2"
+logintitle.TextColor3 = Color3.fromRGB(170, 0, 0)
+logintitle.TextScaled = true
+logintitle.TextSize = 14.000
+logintitle.TextStrokeTransparency = 0.000
+logintitle.TextWrapped = true
+
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)), ColorSequenceKeypoint.new(0.48, Color3.fromRGB(170, 0, 0)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))}
+UIGradient.Rotation = 90
+UIGradient.Parent = loginframe
+
+logintitle2.Name = "logintitle2"
+logintitle2.Parent = loginframe
+logintitle2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+logintitle2.BackgroundTransparency = 1.000
+logintitle2.Position = UDim2.new(0.0945945978, 0, 0.138181821, 0)
+logintitle2.Size = UDim2.new(0, 179, 0, 31)
+logintitle2.Font = Enum.Font.SciFi
+logintitle2.Text = "Login"
+logintitle2.TextColor3 = Color3.fromRGB(170, 0, 0)
+logintitle2.TextScaled = true
+logintitle2.TextSize = 14.000
+logintitle2.TextStrokeTransparency = 0.000
+logintitle2.TextWrapped = true
+
+user.Name = "user"
+user.Parent = loginframe
+user.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+user.Position = UDim2.new(0.0945945978, 0, 0.290909082, 0)
+user.Size = UDim2.new(0, 178, 0, 38)
+user.ZIndex = 2
+user.Font = Enum.Font.SciFi
+user.PlaceholderColor3 = Color3.fromRGB(170, 0, 0)
+user.PlaceholderText = "Username Here"
+user.Text = ""
+user.TextColor3 = Color3.fromRGB(170, 0, 0)
+user.TextScaled = true
+user.TextSize = 14.000
+user.TextStrokeTransparency = 0.000
+user.TextWrapped = true
+
+UICorner_2.Parent = user
+
+DropShadow1.Name = "DropShadow1"
+DropShadow1.Parent = user
+DropShadow1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+DropShadow1.BorderSizePixel = 0
+DropShadow1.Position = UDim2.new(0, 10, 0, 10)
+DropShadow1.Size = UDim2.new(1, 0, 1, 0)
+
+UICorner_3.Parent = DropShadow1
+
+pass.Name = "pass"
+pass.Parent = loginframe
+pass.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+pass.Position = UDim2.new(0.0990990996, 0, 0.516363621, 0)
+pass.Size = UDim2.new(0, 178, 0, 38)
+pass.ZIndex = 2
+pass.Font = Enum.Font.SciFi
+pass.PlaceholderColor3 = Color3.fromRGB(170, 0, 0)
+pass.PlaceholderText = "Password Here"
+pass.Text = ""
+pass.TextColor3 = Color3.fromRGB(170, 0, 0)
+pass.TextScaled = true
+pass.TextSize = 14.000
+pass.TextStrokeTransparency = 0.000
+pass.TextWrapped = true
+
+UICorner_4.Parent = pass
+
+DropShadow1_2.Name = "DropShadow1"
+DropShadow1_2.Parent = pass
+DropShadow1_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+DropShadow1_2.BorderSizePixel = 0
+DropShadow1_2.Position = UDim2.new(0, 10, 0, 10)
+DropShadow1_2.Size = UDim2.new(1, 0, 1, 0)
+
+UICorner_5.Parent = DropShadow1_2
+
+buttonlog.Name = "buttonlog"
+buttonlog.Parent = loginframe
+buttonlog.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+buttonlog.Position = UDim2.new(0.157657653, 0, 0.74545455, 0)
+buttonlog.Size = UDim2.new(0, 151, 0, 35)
+buttonlog.ZIndex = 2
+buttonlog.Font = Enum.Font.SciFi
+buttonlog.Text = "Login"
+buttonlog.TextColor3 = Color3.fromRGB(170, 0, 0)
+buttonlog.TextScaled = true
+buttonlog.TextSize = 14.000
+buttonlog.TextStrokeTransparency = 0.000
+buttonlog.TextWrapped = true
+
+UICorner_6.Parent = buttonlog
+
+DropShadow3.Name = "DropShadow3"
+DropShadow3.Parent = buttonlog
+DropShadow3.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+DropShadow3.BorderSizePixel = 0
+DropShadow3.Position = UDim2.new(0, 10, 0, 10)
+DropShadow3.Size = UDim2.new(1, 0, 1, 0)
+
+UICorner_7.Parent = DropShadow3
+
+openlogin.Name = "openlogin"
+openlogin.Parent = h7843XploitsMM2
+openlogin.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+openlogin.Position = UDim2.new(0.031934306, 0, 0.711729646, 0)
+openlogin.Size = UDim2.new(0, 156, 0, 41)
+openlogin.Font = Enum.Font.SciFi
+openlogin.Text = "Login"
+openlogin.TextColor3 = Color3.fromRGB(170, 0, 0)
+openlogin.TextScaled = true
+openlogin.TextSize = 14.000
+openlogin.TextStrokeTransparency = 0.000
+openlogin.TextWrapped = true
+
+UICorner_8.Parent = openlogin
+
+UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)), ColorSequenceKeypoint.new(0.48, Color3.fromRGB(170, 0, 0)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))}
+UIGradient_2.Rotation = 90
+UIGradient_2.Parent = openlogin
+
+openmm2hax.Name = "openmm2hax"
+openmm2hax.Parent = h7843XploitsMM2
+openmm2hax.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+openmm2hax.Position = UDim2.new(0.031934306, 0, 0.711729646, 0)
+openmm2hax.Size = UDim2.new(0, 156, 0, 41)
+openmm2hax.Visible = false
+openmm2hax.Font = Enum.Font.SciFi
+openmm2hax.Text = "7843 Xploits"
+openmm2hax.TextColor3 = Color3.fromRGB(170, 0, 0)
+openmm2hax.TextScaled = true
+openmm2hax.TextSize = 14.000
+openmm2hax.TextStrokeTransparency = 0.000
+openmm2hax.TextWrapped = true
+
+UICorner_9.Parent = openmm2hax
+
+UIGradient_3.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)), ColorSequenceKeypoint.new(0.48, Color3.fromRGB(170, 0, 0)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))}
+UIGradient_3.Rotation = 90
+UIGradient_3.Parent = openmm2hax
+
+h7843Xploitsmain.Name = "h7843Xploitsmain"
+h7843Xploitsmain.Parent = h7843XploitsMM2
+h7843Xploitsmain.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+h7843Xploitsmain.Position = UDim2.new(-0.606999993, 0, 0.171000004, 0)
+h7843Xploitsmain.Size = UDim2.new(0, 247, 0, 330)
+
+UICorner_10.Parent = h7843Xploitsmain
+
+UIGradient_4.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(0, 0, 0)), ColorSequenceKeypoint.new(0.48, Color3.fromRGB(170, 0, 0)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))}
+UIGradient_4.Rotation = 90
+UIGradient_4.Parent = h7843Xploitsmain
+
+h7843xploitstitle.Name = "h7843xploitstitle"
+h7843xploitstitle.Parent = h7843Xploitsmain
+h7843xploitstitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+h7843xploitstitle.BackgroundTransparency = 1.000
+h7843xploitstitle.Size = UDim2.new(0, 247, 0, 53)
+h7843xploitstitle.Font = Enum.Font.SciFi
+h7843xploitstitle.Text = "7843 Xploits"
+h7843xploitstitle.TextColor3 = Color3.fromRGB(170, 0, 0)
+h7843xploitstitle.TextScaled = true
+h7843xploitstitle.TextSize = 14.000
+h7843xploitstitle.TextStrokeTransparency = 0.000
+h7843xploitstitle.TextWrapped = true
+
+credits.Name = "credits"
+credits.Parent = h7843Xploitsmain
+credits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+credits.BackgroundTransparency = 1.000
+credits.Position = UDim2.new(0, 0, 0.918142438, 0)
+credits.Size = UDim2.new(0, 247, 0, 27)
+credits.Font = Enum.Font.SciFi
+credits.Text = "Made by Weston7843 | 7843 Xploits"
+credits.TextColor3 = Color3.fromRGB(170, 0, 0)
+credits.TextScaled = true
+credits.TextSize = 14.000
+credits.TextStrokeTransparency = 0.000
+credits.TextWrapped = true
+
+eclipse.Name = "eclipse"
+eclipse.Parent = h7843Xploitsmain
+eclipse.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+eclipse.Position = UDim2.new(0.0931174085, 0, 0.172727272, 0)
+eclipse.Size = UDim2.new(0, 200, 0, 38)
+eclipse.Font = Enum.Font.SciFi
+eclipse.Text = "Eclipse UI"
+eclipse.TextColor3 = Color3.fromRGB(170, 0, 0)
+eclipse.TextScaled = true
+eclipse.TextSize = 14.000
+eclipse.TextStrokeTransparency = 0.000
+eclipse.TextWrapped = true
+
+UICorner_11.Parent = eclipse
+
+vynixu.Name = "vynixu"
+vynixu.Parent = h7843Xploitsmain
+vynixu.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+vynixu.Position = UDim2.new(0.0931174085, 0, 0.306060612, 0)
+vynixu.Size = UDim2.new(0, 200, 0, 38)
+vynixu.Font = Enum.Font.SciFi
+vynixu.Text = "Vynixu's MM2 Gui"
+vynixu.TextColor3 = Color3.fromRGB(170, 0, 0)
+vynixu.TextScaled = true
+vynixu.TextSize = 14.000
+vynixu.TextStrokeTransparency = 0.000
+vynixu.TextWrapped = true
+
+UICorner_12.Parent = vynixu
+
+infiniteyeild.Name = "infiniteyeild"
+infiniteyeild.Parent = h7843Xploitsmain
+infiniteyeild.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+infiniteyeild.Position = UDim2.new(0.0931174085, 0, 0.442424238, 0)
+infiniteyeild.Size = UDim2.new(0, 200, 0, 38)
+infiniteyeild.Font = Enum.Font.SciFi
+infiniteyeild.Text = "Infinite Yeild (Admin Commands)"
+infiniteyeild.TextColor3 = Color3.fromRGB(170, 0, 0)
+infiniteyeild.TextScaled = true
+infiniteyeild.TextSize = 14.000
+infiniteyeild.TextStrokeTransparency = 0.000
+infiniteyeild.TextWrapped = true
+
+UICorner_13.Parent = infiniteyeild
+
+emotes.Name = "emotes"
+emotes.Parent = h7843Xploitsmain
+emotes.BackgroundColor3 = Color3.fromRGB(170, 0, 0)
+emotes.Position = UDim2.new(0.0931174085, 0, 0.581818163, 0)
+emotes.Size = UDim2.new(0, 200, 0, 38)
+emotes.Font = Enum.Font.SciFi
+emotes.Text = "Free Emotes"
+emotes.TextColor3 = Color3.fromRGB(170, 0, 0)
+emotes.TextScaled = true
+emotes.TextSize = 14.000
+emotes.TextStrokeTransparency = 0.000
+emotes.TextWrapped = true
+
+UICorner_14.Parent = emotes
+
+-- Scripts:
+
+local function HFWV_fake_script() -- buttonlog.LocalScript 
+	local script = Instance.new('LocalScript', buttonlog)
+
+	local UserData = script.Parent.Parent.user
+	local UserPass = script.Parent.Parent.pass
+	local frame = script.Parent.Parent.Parent.h7843Xploitsmain
+	local buttonlog = script.Parent.Parent.buttonlog
+	local loginframe = script.Parent.Parent.Parent.loginframe
+	local openhax = script.Parent.Parent.Parent.openmm2hax
+	
+	script.Parent.MouseButton1Click:Connect(function()
+		if UserData.Text == "Weston7843" and UserPass.Text == "jasperandlaylaarecute" or
+			UserData.Text == "456" and UserPass.Text == "squidgame" then
+			frame.Visible = true
+			loginframe.Visible = false
+			buttonlog.Visible = false
+			openhax.Visible = true
+		else
+			script.Parent.Parent.buttonlog.Text = "Invalid Info"
+			wait(1.5)
+			script.Parent.Parent.buttonlog.Text = "Try Again"
+			wait(1.0)
+			script.Parent.Parent.buttonlog.Text = "Login"
+		end
+	end)
+end
+coroutine.wrap(HFWV_fake_script)()
+local function KGMNIOD_fake_script() -- openlogin.animationlogin 
+	local script = Instance.new('Script', openlogin)
+
+	wait(1)
+	local t = script.Parent.Parent.openlogin
+	local on = false
+	local f = script.Parent.Parent.loginframe
+	script.Parent.MouseButton1Click:Connect(function()
+		if on == false then on = true
+			f:TweenPosition(UDim2.new(0.688, 0,0.286, 0),"Out","Bounce",1)
+			t.Text = "Close Login"
+		else
+			on = false
+			f:TweenPosition(UDim2.new(-0.688, 0,0.286, 0),"Out","Bounce",1)
+			t.Text = "Login"
+		end
+	end)
+	
+	script.Parent.MouseButton1Click:Connect(function(click)
+		
+	end)
+end
+coroutine.wrap(KGMNIOD_fake_script)()
+local function ITVWWZS_fake_script() -- openmm2hax.animation 
+	local script = Instance.new('Script', openmm2hax)
+
+	wait(1)
+	local t = script.Parent.Parent.openmm2hax
+	local on = false
+	local f = script.Parent.Parent.h7843Xploitsmain
+	script.Parent.MouseButton1Click:Connect(function()
+		if on == false then on = true
+			f:TweenPosition(UDim2.new(0.611, 0,0.171, 0),"Out","Bounce",1)
+			t.Text = "Close 7843 Xploits"
+		else
+			on = false
+			f:TweenPosition(UDim2.new(-0.611, 0,0.171, 0),"Out","Bounce",1)
+			t.Text = "7843 Xploits"
+		end
+	end)
+	
+end
+coroutine.wrap(ITVWWZS_fake_script)()
+local function ACJBARE_fake_script() -- eclipse.LocalScript 
+	local script = Instance.new('LocalScript', eclipse)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))() 
+	end)
+end
+coroutine.wrap(ACJBARE_fake_script)()
+local function MFRO_fake_script() -- vynixu.LocalScript 
+	local script = Instance.new('LocalScript', vynixu)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+	end)
+end
+coroutine.wrap(MFRO_fake_script)()
+local function GQXD_fake_script() -- infiniteyeild.LocalScript 
+	local script = Instance.new('LocalScript', infiniteyeild)
+
+	local button = script.Parent
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+	end)
+end
+coroutine.wrap(GQXD_fake_script)()
+local function JFKUIH_fake_script() -- emotes.LocalScript 
+	local script = Instance.new('LocalScript', emotes)
+
+	local button = script.Parent
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/xcjzjzr8", true))() 
+	end)
+end
+coroutine.wrap(JFKUIH_fake_script)()
